99d2597de64c9e5c88bd094f357e19289535c7c5
wangyu <wy@terminus.io>
2016-11-29 19:03:09 +0800

    init

--- Greeter.js
+++ Greeter.js
@@ -1,6 +1,7 @@
+var config = require('./config.json');
 // Greeter.js
 module.exports = function() {
   var greet = document.createElement('div');
-  greet.textContent = "Hi there and greetings!";
+  greet.textContent = config.greetText;
   return greet;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,30 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
+  devtool: 'source-map',
   entry: __dirname + "/main.js",
   output: {
-    path: __dirname,
+    path: __dirname + "/public",
     filename: "bundle.js"
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json"
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./public",//本地服务器所加载的页面所在的目录
+    port: 3000,
+    colors: true,//终端中输出结果为彩色
+    historyApiFallback: true,//不跳转
+    inline: true//实时刷新
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './index.html' },
+    ])
+  ]
 }
